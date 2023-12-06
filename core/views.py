b96441109f32c461035163be37b31c42abb34f63
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-12-06 19:52:25 -0300

    arquivos staticos

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Produto
-
+from django.template import loader
+from django.http import HttpResponse
 
 def index(request):
     produtos = Produto.objects.all()
@@ -17,8 +19,17 @@ def contato(request):
 
 
 def produto(request, pk):
-    prod = Produto.objects.get(id=pk)
+    #prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto, id=pk)
     context = {
         'produto': prod
     }
     return render(request, 'produto.html', context)
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html: charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html: charset=utf8', status=500)
--- django1/settings.py
+++ django1/settings.py
@@ -116,3 +116,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # usado dirante a produçã
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGOUT_REDIRECT_URL = 'index'
