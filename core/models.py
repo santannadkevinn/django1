fe293ec29bb5b3321aca6f350779684e969f9976
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-12-06 18:46:32 -0300

    trabalhando com arquivos estaticos

--- core/models.py
+++ core/models.py
@@ -5,8 +5,10 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
+
     def __str__(self):
         return self.nome
+
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
